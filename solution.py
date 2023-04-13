3a6f9444ed8a14702871f75edab1b6369e5a2523
VictorIrinin <129628121+VictorIrinin@users.noreply.github.com>
2023-04-13 17:21:16 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,7 +9,7 @@ def solution(x: np.array, y: np.array) -> bool:
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     res = False
-    if x[0] == y[0] and x[1] == y[1] and x[2] == y[2]:
+    if x[0] == y[0] and x[1] == y[1] and x[2] == y[2] and x[3] == y[3] and x[4] == y[4]:
         res = False
     else:
         res = True
